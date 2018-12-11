58133ed5b06eb7fcf973c73c092179c1690705f5
moi07 <mjordash@gmail.com>
2018-12-11 13:53:35 +0200

    react code

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Projects from './Components/Projects';
+import AddProject from './Components/AddProject';
 import './App.css';
 
 class App extends Component {
+    constructor() { 
+        super(); 
+        this.state = {
+            projects: []    
+        }
+    }
+
+    componentWillMount() { 
+        this.setState({projects:[
+            {
+                title: 'Business Website',
+                category: 'Web Design'
+            },
+            {
+                title: 'Social App',
+                category: 'Mobile Development'
+            },
+            {
+                title: 'Ecommerce Shopping Cart',
+                category: 'Web Development'
+            }
+        ]});
+    }
+
     render() {
         return ( 
         <div className = "App" >
-            My App 
+            <AddProject />
+            <Projects projects={this.state.projects}/>   
         </div>
         );
     }
